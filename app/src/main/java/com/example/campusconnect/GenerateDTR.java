397f81cb1253dd635f2740d0ae3957def1485671
Administrator <mark.jayson.aguda@gmail.com>
2023-02-16 10:00:50 +0800

    layout dtr

--- app/src/main/java/com/example/campusconnect/GenerateDTR.java
+++ app/src/main/java/com/example/campusconnect/GenerateDTR.java
@@ -6,10 +6,18 @@ import static java.lang.Integer.parseInt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -30,6 +38,12 @@ public class GenerateDTR extends AppCompatActivity {
 
     SaveData save = SaveData.getInstance();
     Read read = new Read();
+
+    private static final int PERMISSION_REQUEST_CODE = 10;
+    //private static final String TAG
+    int pdfHeight = 1080;
+    int pdfWidth = 720;
+    private PdfDocument document;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,13 +97,25 @@ public class GenerateDTR extends AppCompatActivity {
                 String year = yearSpinner.getSelectedItem().toString();
                 int day = DateUtils.getNumberOfDays(year, month);
 
-                // TODO PDF View
-                WebView webView = findViewById(R.id.web_view);
-                webView.loadUrl("F:/Miner/Documents/MJDMiner Papers/Resume/Mark Jayson Molina Aguda - Resume.pdf");
+                final Dialog dtrdialog = new Dialog(GenerateDTR.this);
+                dtrdialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dtrdialog.setContentView(R.layout.activity_dtr_layout);
+                dtrdialog.setCancelable(true);
+                WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+                lp.copyFrom(dtrdialog.getWindow().getAttributes());
+                lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+                lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+                dtrdialog.getWindow().setAttributes(lp);
+                Button downloaddtr = dtrdialog.findViewById(R.id.downloadDTR_Button);
 
+                dtrdialog.show();
+
+                downloaddtr.setOnClickListener(v1 ->{
+                    generatePdfView(dtrdialog.findViewById(R.id.scrollView));
+                });
 
                 // TODO do not delete this you will be able to use this code later on
-                for(int i = 1 ; i <= day ; i++) {
+                /*for(int i = 1 ; i <= day ; i++) {
                     int j = i;
                     read.readRecord("employee/" + save.getId() + "/attendance/" + save.getYear() + "/" + save.getMonth() + "/" + i, new Read.OnGetDataListener() {
                         @Override
@@ -117,6 +143,41 @@ public class GenerateDTR extends AppCompatActivity {
                         }
                     });
                 }
+
+                 */
+            }
+
+            private void generatePdfView(View view) {
+                Bitmap bitmap = getBitmapView(view);
+                document = new PdfDocument();
+                PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(bitmap.getWidth(), bitmap.getHeight(), 1).create();
+                PdfDocument.Page myPage = document.startPage(mypageInfo);
+                Canvas canvas = myPage.getCanvas();
+                canvas.drawBitmap(bitmap,0,0,null);
+                document.finishPage(myPage);
+                createFile();
+            }
+
+            private void createFile() {
+                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("application/pdf");
+                intent.putExtra(Intent.EXTRA_TITLE, "dtr.pdf");
+                startActivityForResult(intent, CREATE_FILE);
+            }
+
+            private Bitmap getBitmapView(View view) {
+                Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(returnedBitmap);
+                Drawable bgDrawable = view.getBackground();
+                if(bgDrawable != null){
+                    bgDrawable.draw(canvas);
+                }
+                else{
+                    canvas.drawColor(Color.WHITE);
+                }
+                view.draw(canvas);
+                return returnedBitmap;
             }
         });
     }
