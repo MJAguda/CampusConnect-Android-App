aa386dc741882f0a0eb9cdcda4b93c8986d8d596
Miner <mark.jayson.aguda@gmail.com>
2023-03-01 19:11:19 +0800

    Generate TAMS

---
+++ app/src/main/java/com/example/campusconnect/GenerateTAMS.java
@@ -0,0 +1,17 @@
+package com.example.campusconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GenerateTAMS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_tams);
+
+        // TODO TAMS XML Design
+
+    }
+}
