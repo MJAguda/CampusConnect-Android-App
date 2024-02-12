a123579973d47cf1d503623df538c34f8b6c150d
MJDMiner <mark.jayson.aguda@gmail.com>
2024-02-12 21:18:24 +0800

    Check Developer Option and GPS location

--- app/src/main/java/com/ams/campusconnect/SplashActivity.java
+++ app/src/main/java/com/ams/campusconnect/SplashActivity.java
@@ -3,6 +3,7 @@ package com.ams.campusconnect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
@@ -39,32 +40,63 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
 
-                if (checkDevOpsOrUnknownSources()) {
-                    String message = "Turn Off 'Developer Option' and 'Unknown Source' settings.";
-                    Toast.makeText(SplashActivity.this, message, Toast.LENGTH_LONG).show();
-
-                    // Close the app
+                if (checkDeveloperOptions()) {
+                    // Developer options are enabled, open settings
                     finish();
-                    return;
+                    openDeveloperOptionsSettings();
                 }
-
-                Intent intent = new Intent(SplashActivity.this, SchoolLogIn.class);
-                startActivity(intent);
-                finish();
+//                else if (checkUnknownSources()) {
+//                    finish();
+//                    openUnknownSourcesSettings();
+//                }
+                else if (!checkGPS()) {
+                    // GPS is disabled, close the app and open settings
+                    finish();
+                    openGPSSettings();
+                }
+                else {
+                    Intent intent = new Intent(SplashActivity.this, SchoolLogIn.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
         }, 5000);
     }
 
-    private boolean checkDevOpsOrUnknownSources() {
-        int developerOptions = Settings.Secure.getInt(getContentResolver(), Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0);
-        int unknownSources;
-        try {
-            unknownSources = Settings.Secure.getInt(getContentResolver(), Settings.Secure.INSTALL_NON_MARKET_APPS);
-        } catch (Settings.SettingNotFoundException e) {
-            e.printStackTrace();
-            unknownSources = 0;
-        }
-        return developerOptions == 1 || unknownSources == 1;
+    private boolean checkDeveloperOptions() {
+        int developerOptions = Settings.Secure.getInt(getContentResolver(), Settings.Global.DEVELOPMENT_SETTINGS_ENABLED , 0);
+        return developerOptions == 1;
+    }
+
+    private boolean checkUnknownSources() {
+        int unknownSources = Settings.Secure.getInt(getContentResolver(), Settings.Secure.INSTALL_NON_MARKET_APPS, 0);
+        return unknownSources == 1;
+    }
+
+    private boolean checkGPS() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    private void openDeveloperOptionsSettings() {
+        Intent intent = new Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS);
+        startActivity(intent);
+        // Display a message informing the user
+        Toast.makeText(SplashActivity.this, "Please turn off Developer Options", Toast.LENGTH_LONG).show();
+    }
+
+    private void openUnknownSourcesSettings() {
+        Intent intent = new Intent(Settings.ACTION_SECURITY_SETTINGS);
+        startActivity(intent);
+        // Display a message informing the user
+        Toast.makeText(SplashActivity.this, "Please turn off Unknown Sources", Toast.LENGTH_LONG).show();
+    }
+
+    private void openGPSSettings() {
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        // Display a message informing the user
+        Toast.makeText(SplashActivity.this, "Please turn on GPS", Toast.LENGTH_LONG).show();
     }
 
     private void getAnnoucement() {
