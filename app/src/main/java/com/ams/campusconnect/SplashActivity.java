87d946d71b8292b10741db756eb000cb48d02165
MJDMiner <mark.jayson.aguda@gmail.com>
2024-02-12 20:06:04 +0800

    Check Developer Option and Unknown Sources

--- app/src/main/java/com/ams/campusconnect/SplashActivity.java
+++ app/src/main/java/com/ams/campusconnect/SplashActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ams.campusconnect.firebase.Read;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 
@@ -21,11 +24,50 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // Check if developer options are enabled
+
+
         // Declare thankyou sound
         final MediaPlayer welcome = MediaPlayer.create(this, R.raw.welcometocampusconnect);
         welcome.start();
 
         // Fetch the Announcement in the Database
+        getAnnoucement();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                if (checkDevOpsOrUnknownSources()) {
+                    String message = "Turn Off 'Developer Option' and 'Unknown Source' settings.";
+                    Toast.makeText(SplashActivity.this, message, Toast.LENGTH_LONG).show();
+
+                    // Close the app
+                    finish();
+                    return;
+                }
+
+                Intent intent = new Intent(SplashActivity.this, SchoolLogIn.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+
+    private boolean checkDevOpsOrUnknownSources() {
+        int developerOptions = Settings.Secure.getInt(getContentResolver(), Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0);
+        int unknownSources;
+        try {
+            unknownSources = Settings.Secure.getInt(getContentResolver(), Settings.Secure.INSTALL_NON_MARKET_APPS);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+            unknownSources = 0;
+        }
+        return developerOptions == 1 || unknownSources == 1;
+    }
+
+    private void getAnnoucement() {
         read.readRecord("Announcement", new Read.OnGetDataListener() {
             @Override
             public void onSuccess(DataSnapshot dataSnapshot) {
@@ -39,15 +81,5 @@ public class SplashActivity extends AppCompatActivity {
                 // handle error here
             }
         });
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, SchoolLogIn.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 5000);
     }
 }
