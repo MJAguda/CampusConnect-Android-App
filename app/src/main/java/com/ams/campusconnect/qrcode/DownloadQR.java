1084fa6cb65be659dd7bc0eabf20d47906842b9d
MJDMiner <mark.jayson.aguda@gmail.com>
2024-03-11 14:07:38 +0800

    Fix DownloadQR Bug

--- app/src/main/java/com/ams/campusconnect/qrcode/DownloadQR.java
+++ app/src/main/java/com/ams/campusconnect/qrcode/DownloadQR.java
@@ -2,55 +2,74 @@ package com.ams.campusconnect.qrcode;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Environment;
+import android.os.storage.StorageManager;
+import android.os.storage.StorageVolume;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.ams.campusconnect.model.Employee;
 import com.ams.campusconnect.model.School;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class DownloadQR {
 
-    static Employee employee = Employee.getInstance();
-    static School school = School.getInstance();
+    private static Employee employee = Employee.getInstance();
+    private static School school = School.getInstance();
 
     private static final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 1;
     private ImageView imageView;
-    private String fileName;
+
     private Bitmap bitmap;
-    private FileOutputStream outputStream;
 
     public DownloadQR(ImageView imageView) {
         this.imageView = imageView;
     }
 
     // TODO add sending Gmail
-    public void downloadImage() {
-        fileName = "QRCode_" + school.getSchoolID() + "_" + employee.getId() + "_" + employee.getFullName() + ".png";
+    public void downloadImage(Context context) {
+        StorageManager storageManager = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
+        if (storageManager == null) {
+            Toast.makeText(context, "Storage manager is not available", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        StorageVolume storageVolume = null; // internal Storage
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            storageVolume = storageManager.getStorageVolumes().get(0);
+        }
+
+        File fileImage = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            fileImage = new File(storageVolume.getDirectory().getPath() + "/Download/" + "QRCode_" +
+                    school.getSchoolID() + "_" + employee.getId() + "_" + employee.getFullName() + ".png");
+        }
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
         bitmap = drawable.getBitmap();
-        if (ContextCompat.checkSelfPermission(imageView.getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions((Activity) imageView.getContext(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_WRITE_EXTERNAL_STORAGE);
-        } else {
-            try {
-                File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName);
-                outputStream = new FileOutputStream(file);
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-                outputStream.close();
-                Toast.makeText(imageView.getContext(), "Image saved to " + file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] bytesArray = byteArrayOutputStream.toByteArray();
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(fileImage);
+            fileOutputStream.write(bytesArray);
+            fileOutputStream.close();
+            Toast.makeText(context, "Image saved to " + fileImage.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(context, "Failed to save image", Toast.LENGTH_SHORT).show();
         }
     }
 }
