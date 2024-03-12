8122794fa355ee4b9dabbd1672fb02dcb08b17fb
MJDMiner <mark.jayson.aguda@gmail.com>
2024-03-12 18:13:48 +0800

    Added SchoolRepositoryTest.java

--- app/src/test/java/com/ams/campusconnect/repository/SchoolRepositoryTest.java
+++ app/src/test/java/com/ams/campusconnect/repository/SchoolRepositoryTest.java
@@ -1,4 +1,36 @@
 package com.ams.campusconnect.repository;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.ams.campusconnect.model.SchoolModel;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 public class SchoolRepositoryTest {
+//    @Mock
+//    FirebaseDatabase mockFirebaseDatabase;
+//
+//    @Mock
+//    DatabaseReference mockDatabaseReference;
+//
+//    @Mock
+//    OnSuccessListener<Void> mockOnSuccessListener;
+//
+//    private SchoolRepository schoolRepository;
+//
+//    @Before
+//    public void setUp(){
+//        MockitoAnnotations.initMocks(this);
+//
+//    }
+
 }
