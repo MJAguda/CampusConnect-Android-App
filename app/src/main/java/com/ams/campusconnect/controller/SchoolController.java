18df0551ecddd88a1d5cec613a4a082b3616243b
MJAguda <mark.jayson.aguda@gmail.com>
2024-03-13 15:04:07 +0800

    Added getSchoolData Method

--- app/src/main/java/com/ams/campusconnect/controller/SchoolController.java
+++ app/src/main/java/com/ams/campusconnect/controller/SchoolController.java
@@ -1,25 +1,39 @@
 package com.ams.campusconnect.controller;
 
+import androidx.annotation.NonNull;
+
+import com.ams.campusconnect.model.SchoolModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class SchoolController {
+    private DatabaseReference databaseReference;
+
+    public SchoolController(int schoolID){
+        databaseReference = FirebaseDatabase.getInstance().getReference().child(schoolID + "/");
+    }
+
+    public void getSchoolData(final OnDataFetchListener listener){
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                SchoolModel schoolModel = snapshot.getValue(SchoolModel.class);
+                listener.onDataFetched(schoolModel);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                listener.onError(error.getMessage());
+            }
+        });
+    }
+
+    public interface OnDataFetchListener{
+        void onDataFetched(SchoolModel schoolModel);
+        void onError(String errorMessage);
+    }
 }
 
-//import java.util.List;
-//
-//public class SchoolController {
-//    private SchoolService schoolService;
-//
-//    public SchoolController(SchoolService schoolService) {
-//        this.schoolService = schoolService;
-//    }
-//
-//    public void addSchool(int id, String name, String address) {
-//        School school = new School(id, name, address);
-//        schoolService.addSchool(school);
-//    }
-//
-//    public List<School> getAllSchools() {
-//        return schoolService.getAllSchools();
-//    }
-//
-//    // Additional methods for handling user interactions and business logic
-//}
