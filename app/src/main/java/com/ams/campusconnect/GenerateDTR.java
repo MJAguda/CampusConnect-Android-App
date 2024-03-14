716807f1c9cc5b458a5ed260085f3aea06028975
MJAguda <mark.jayson.aguda@gmail.com>
2024-03-14 15:29:15 +0800

    Relocate the instace of DTR and implement it in download

--- app/src/main/java/com/ams/campusconnect/GenerateDTR.java
+++ app/src/main/java/com/ams/campusconnect/GenerateDTR.java
@@ -86,6 +86,8 @@ public class GenerateDTR extends AppCompatActivity {
             startActivity(intent);
         });
 
+        DTR dtr = new DTR(schoolModel);
+
         // Generate DTR
         generate.setOnClickListener(view -> {
 
@@ -98,11 +100,11 @@ public class GenerateDTR extends AppCompatActivity {
             //int day = DateUtils.getNumberOfDays(save.getMonth(), save.getYear());
             int day = DateUtils.getNumberOfDays(month, year);
 
-            DTR dtr = new DTR(schoolModel);
+
 
             dtr.generateDTR(employee.getId(), month, day, year, name, date, schoolHead, table, GenerateDTR.this);
         });
 
-        download.setOnClickListener(view -> DTR.downloadDTR(findViewById(R.id.dtr_LinearLayout), GenerateDTR.this));
+        download.setOnClickListener(view -> dtr.downloadDTR(findViewById(R.id.dtr_LinearLayout), GenerateDTR.this));
     }
 }
