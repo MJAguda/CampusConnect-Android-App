42fe4d23a69f92b0175aab5d8474af828600ad2e
MJDMiner <mark.jayson.aguda@gmail.com>
2024-03-26 15:47:20 +0800

    Added and commented getAllSchools that returns a List of School Object

--- app/src/main/java/com/ams/campusconnect/controller/SchoolController.java
+++ app/src/main/java/com/ams/campusconnect/controller/SchoolController.java
@@ -1,6 +1,7 @@
 package com.ams.campusconnect.controller;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 
@@ -13,6 +14,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class SchoolController {
     private DatabaseReference databaseReference;
@@ -44,5 +49,37 @@ public class SchoolController {
     public void getAllSchoolIDs(final SchoolRepository.OnDataFetchListener listener){
         schoolService.getAllSchoolIDs(listener);
     }
+
+    public void getAllSchools(final SchoolRepository.OnDataFetchListener listener){
+        schoolService.getAllSchools(listener);
+    }
+
+//    public List<School> getAllSchools (){
+//        List<School> schools = new ArrayList<>();
+//        schoolService.getAllSchools(new SchoolRepository.OnDataFetchListener() {
+//            @Override
+//            public void onSuccess(DataSnapshot dataSnapshot) {
+//                // Get all schools and their fields and set it to School class
+//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+//                    if (Objects.requireNonNull(snapshot.getKey()).matches("[0-9]+")) {
+//                        // get children of school
+//                        School school = snapshot.getValue(School.class);
+//                        schools.add(school);
+//                    }
+//                }
+//
+//                // Traverse List of Schools
+//                for (School school : schools) {
+//                    Log.d("School", school.getSchoolName());
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(DatabaseError databaseError) {
+//                Log.e("SchoolController", "Error fetching schools", databaseError.toException());
+//            }
+//        });
+//        return schools;
+//    }
 }
 
