199482b0668f1a2ebe719edf6bad031029400c14
MJDMiner <mark.jayson.aguda@gmail.com>
2024-04-05 23:51:45 +0800

    Added and designed TimelogChangeActivity

---
+++ app/src/main/java/com/ams/campusconnect/TimelogChangeActivity.java
@@ -0,0 +1,14 @@
+package com.ams.campusconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimelogChangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timelog_change);
+    }
+}
