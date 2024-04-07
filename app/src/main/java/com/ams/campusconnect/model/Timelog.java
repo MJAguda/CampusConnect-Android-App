bc3c8b07cb6ad53b4359c46a01a507b91854d672
MJDMiner <mark.jayson.aguda@gmail.com>
2024-04-07 23:32:55 +0800

    Change and added field datatypes

--- app/src/main/java/com/ams/campusconnect/model/Timelog.java
+++ app/src/main/java/com/ams/campusconnect/model/Timelog.java
@@ -1,31 +1,28 @@
 package com.ams.campusconnect.model;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Timelog {
-
     private int requestorID;
     private int requestorSchoolID;
     private String typeOfTimeLogIssue;
-    private Date incorrectTimeLogDate;
-    private Date correctTimeLogDate;
+    private LocalDate date;
+    private String timelogSession;
+    private LocalTime correctTimelog; // This is in 07:00 AM format (HH:mm a)
     private String reasonForChange;
 
-    // Constructor
-    public Timelog(int requestorID, int requestorSchoolID, String typeOfTimeLogIssue, Date incorrectTimeLogDate, Date correctTimeLogDate, String reasonForChange) {
+    public Timelog(int requestorID, int requestorSchoolID, String typeOfTimeLogIssue, LocalDate date, String timelogSession, LocalTime correctTimelog, String reasonForChange) {
         this.requestorID = requestorID;
         this.requestorSchoolID = requestorSchoolID;
         this.typeOfTimeLogIssue = typeOfTimeLogIssue;
-        this.incorrectTimeLogDate = incorrectTimeLogDate;
-        this.correctTimeLogDate = correctTimeLogDate;
+        this.date = date;
+        this.timelogSession = timelogSession;
+        this.correctTimelog = correctTimelog;
         this.reasonForChange = reasonForChange;
     }
 
-    // Empty Constructor
-    public Timelog() {
-    }
-
-    // Getters
+    public Timelog(){}
 
     public int getRequestorID() { return requestorID; }
 
@@ -33,14 +30,13 @@ public class Timelog {
 
     public String getTypeOfTimeLogIssue() { return typeOfTimeLogIssue; }
 
-    public Date getIncorrectTimeLogDate() { return incorrectTimeLogDate; }
+    public LocalDate getDate() { return date; }
 
-    public Date getCorrectTimeLogDate() { return correctTimeLogDate; }
+    public String getTimelogSession() { return timelogSession; }
+
+    public LocalTime getCorrectTimelog() { return correctTimelog; }
 
     public String getReasonForChange() { return reasonForChange; }
-
-
-    // Setters
 
     public void setRequestorID(int requestorID) { this.requestorID = requestorID; }
 
@@ -48,9 +44,24 @@ public class Timelog {
 
     public void setTypeOfTimeLogIssue(String typeOfTimeLogIssue) { this.typeOfTimeLogIssue = typeOfTimeLogIssue; }
 
-    public void setIncorrectTimeLogDate(Date incorrectTimeLogDate) { this.incorrectTimeLogDate = incorrectTimeLogDate; }
+    public void setDate(LocalDate date) { this.date = date; }
 
-    public void setCorrectTimeLogDate(Date correctTimeLogDate) { this.correctTimeLogDate = correctTimeLogDate; }
+    public void setTimelogSession(String timelogSession) { this.timelogSession = timelogSession; }
+
+    public void setCorrectTimelog(LocalTime correctTimelog) { this.correctTimelog = correctTimelog; }
 
     public void setReasonForChange(String reasonForChange) { this.reasonForChange = reasonForChange; }
+
+    @Override
+    public String toString() {
+        return "Timelog{" +
+                "requestorID=" + requestorID +
+                ", requestorSchoolID=" + requestorSchoolID +
+                ", typeOfTimeLogIssue='" + typeOfTimeLogIssue + '\'' +
+                ", date=" + date +
+                ", timelogSession=" + timelogSession +
+                ", correctTimelog=" + correctTimelog +
+                ", reasonForChange='" + reasonForChange + '\'' +
+                '}';
+    }
 }
