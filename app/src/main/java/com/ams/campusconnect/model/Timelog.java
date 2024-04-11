fcb178942843ee27b9b3958b84dcb15af195552a
MJDMiner <mark.jayson.aguda@gmail.com>
2024-04-11 18:47:29 +0800

    Added requestID and changed date and correctTimelog datatypes to String

--- app/src/main/java/com/ams/campusconnect/model/Timelog.java
+++ app/src/main/java/com/ams/campusconnect/model/Timelog.java
@@ -4,19 +4,24 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Timelog {
-    private int requestorID;
+    private String requestID;
+    private String requestorID;
     private int requestorSchoolID;
     private String typeOfTimeLogIssue;
-    private LocalDate date;
+    private String month, day, year;
     private String timelogSession;
-    private LocalTime correctTimelog; // This is in 07:00 AM format (HH:mm a)
+    private String correctTimelog;
+
     private String reasonForChange;
 
-    public Timelog(int requestorID, int requestorSchoolID, String typeOfTimeLogIssue, LocalDate date, String timelogSession, LocalTime correctTimelog, String reasonForChange) {
+    public Timelog(String requestID, String requestorID, int requestorSchoolID, String typeOfTimeLogIssue, String month, String day, String year, String timelogSession, String correctTimelog, String reasonForChange) {
+        this.requestID = requestID;
         this.requestorID = requestorID;
         this.requestorSchoolID = requestorSchoolID;
         this.typeOfTimeLogIssue = typeOfTimeLogIssue;
-        this.date = date;
+        this.month = month;
+        this.day = day;
+        this.year = year;
         this.timelogSession = timelogSession;
         this.correctTimelog = correctTimelog;
         this.reasonForChange = reasonForChange;
@@ -24,43 +29,41 @@ public class Timelog {
 
     public Timelog(){}
 
-    public int getRequestorID() { return requestorID; }
-
+    // Getters
+    public String getRequestID() { return requestID; }
+    public String getRequestorID() { return requestorID; }
     public int getRequestorSchoolID() { return requestorSchoolID; }
-
     public String getTypeOfTimeLogIssue() { return typeOfTimeLogIssue; }
-
-    public LocalDate getDate() { return date; }
-
+    public String getMonth() { return month; }
+    public String getDay() { return day; }
+    public String getYear() { return year; }
     public String getTimelogSession() { return timelogSession; }
-
-    public LocalTime getCorrectTimelog() { return correctTimelog; }
-
+    public String getCorrectTimelog() { return correctTimelog; }
     public String getReasonForChange() { return reasonForChange; }
 
-    public void setRequestorID(int requestorID) { this.requestorID = requestorID; }
-
+    // Setters
+    public void setRequestID(String requestID) { this.requestID = requestID; }
+    public void setRequestorID(String requestorID) { this.requestorID = requestorID; }
     public void setRequestorSchoolID(int requestorSchoolID) { this.requestorSchoolID = requestorSchoolID; }
-
     public void setTypeOfTimeLogIssue(String typeOfTimeLogIssue) { this.typeOfTimeLogIssue = typeOfTimeLogIssue; }
-
-    public void setDate(LocalDate date) { this.date = date; }
-
+    public void setMonth(String month) { this.month = month; }
+    public void setDay(String day) { this.day = day; }
+    public void setYear(String year) { this.year = year; }
     public void setTimelogSession(String timelogSession) { this.timelogSession = timelogSession; }
-
-    public void setCorrectTimelog(LocalTime correctTimelog) { this.correctTimelog = correctTimelog; }
-
+    public void setCorrectTimelog(String correctTimelog) { this.correctTimelog = correctTimelog; }
     public void setReasonForChange(String reasonForChange) { this.reasonForChange = reasonForChange; }
 
     @Override
     public String toString() {
         return "Timelog{" +
-                "requestorID=" + requestorID +
+                "requestorID='" + requestorID + '\'' +
                 ", requestorSchoolID=" + requestorSchoolID +
                 ", typeOfTimeLogIssue='" + typeOfTimeLogIssue + '\'' +
-                ", date=" + date +
-                ", timelogSession=" + timelogSession +
-                ", correctTimelog=" + correctTimelog +
+                ", month='" + month + '\'' +
+                ", day='" + day + '\'' +
+                ", year='" + year + '\'' +
+                ", timelogSession='" + timelogSession + '\'' +
+                ", correctTimelog='" + correctTimelog + '\'' +
                 ", reasonForChange='" + reasonForChange + '\'' +
                 '}';
     }
